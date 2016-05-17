d10e46b8cb94f64017118c464cbeb1a5f06466bd
VictorParraCant <victor.parracant@gmail.com>
2016-05-17 19:54:54 +0200

    Html basico

---
+++ javascripts/app.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+  $('.navbar-nav').find('li').off('click').on('click', function(e){
+    e.preventDefault();
+    var direcction = $(this).find('a').attr('href');
+    $.scrollTo( direcction, 900, {axis:'y', offset:-81} );
+  });
+});
