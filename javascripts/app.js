642125cfadc20df6d15a9af5e02756a5ff61da0f
VictorParraCant <victor.parracant@gmail.com>
2016-05-19 13:13:52 +0200

    Nueva agenda y enlaces

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
   $('.navbar-nav').find('li').off('click').on('click', function(e){
     e.preventDefault();
     var direcction = $(this).find('a').attr('href');
-    $.scrollTo( direcction, 900, {axis:'y', offset:-81} );
+    $.scrollTo( direcction, 900, {axis:'y', offset:-95} );
   });
 });
